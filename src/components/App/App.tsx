345cbc9dee9d1f2a79bd72d2e10c7420d9547b4e
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-16 20:34:41 +0300

    add auth, main components to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Box, CssBaseline } from '@mui/material';
 import Auth from '../Auth/Auth';
+import Main from '../Main/Main';
 
 export default function App() {
   return (
-    <Box sx={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyConent: 'center',
-      minHeight: '100vh',
-      backgroundColor: '#d8f4cc',
-    }}>
+    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
       <CssBaseline />
-      <Auth />
+      {/* <Auth /> */}
+      <Main />
     </Box>
   );
 }
