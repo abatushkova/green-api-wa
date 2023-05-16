2e0aa30731b4cc11fd02b8b611f44a2ff62d8e52
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-16 18:35:42 +0300

    add themes to index, update fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './components/App/App';
+import { theme } from './themes';
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/themes/index.ts
@@ -0,0 +1,113 @@
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    white: Palette['primary'];
+    gray: Palette['primary'];
+  }
+  interface PaletteOptions {
+    white: PaletteOptions['primary'];
+    gray: PaletteOptions['primary'];
+  }
+}
+
+export let theme = createTheme({
+  palette: {
+    primary: {
+      light: '#57975b',
+      main: '#2e7d32',
+      dark: '#205723',
+      contrastText: '#fff',
+    },
+    gray: {
+      main: '#f4f5f7',
+      dark: '#aaabac',
+    },
+    white: {
+      main: '#fff',
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  components: {
+    MuiTab: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
+});
+
+theme = {
+  ...theme,
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          '&.MuiTypography-h1': {
+            fontSize: 32,
+            fontWeight: 700,
+            [theme.breakpoints.up('md')]: {
+              fontSize: 45,
+            },
+          },
+        },
+      },
+    },
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: theme.palette.primary.dark,
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: theme.palette.primary.dark,
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+        contained: {
+          boxShadow: 'none',
+          '&:active': {
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          padding: theme.spacing(1),
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '& .MuiInputLabel-root': {
+            color: theme.palette.gray.dark,
+          },
+          '& .MuiInputBase-multiline': {
+            backgroundColor: theme.palette.primary.main,
+          }
+        },
+      }
+    },
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+          width: 32,
+          height: 32,
+        },
+      },
+    },
+  },
+}
