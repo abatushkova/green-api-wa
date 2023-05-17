b0fb209a5ab5475c09b7379b25b2af574079fce6
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-17 21:44:03 +0300

    add delete-button to contact-item component

--- src/components/ContactItem/ContactItem.tsx
+++ src/components/ContactItem/ContactItem.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import {
   ListItem,
   ListItemButton,
   ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
 } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useAppDispatch } from '../../app/hooks';
+import { deleteContact } from '../../features/contacts/contactsSlice';
+
+const CustomListItem = styled(ListItem)(({ theme }) => ({
+  '&.MuiListItem-secondaryAction': {
+    paddingRight: 0,
+  },
+}));
 
 interface IContactProps {
-  number: number;
+  number: string;
   selected: number;
   index: number;
   onContactClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>, S: number) => void;
@@ -14,17 +26,29 @@ interface IContactProps {
 
 export default function ContactItem(props: IContactProps) {
   const { index, number, selected, onContactClick } = props;
+  const dispatch = useAppDispatch();
+
+  const handleDelete = () => dispatch(deleteContact(number));
 
   return (
-    <>
-      <ListItem disablePadding>
-        <ListItemButton
-          selected={selected === index}
-          onClick={(e) => onContactClick(e, index)}
+    <CustomListItem disablePadding>
+      <ListItemButton
+        selected={selected === index}
+        onClick={(e) => onContactClick(e, index)}
+      >
+        <ListItemText>{number}</ListItemText>
+      </ListItemButton>
+      <ListItemSecondaryAction className="hidden-menu">
+        <IconButton
+          edge="end"
+          aria-label="Delete"
+          size="small"
+          onClick={handleDelete}
+          sx={{ mr: 0.5 }}
         >
-          <ListItemText>{number}</ListItemText>
-        </ListItemButton>
-      </ListItem>
-    </>
+          <DeleteIcon fontSize="inherit" />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </CustomListItem>
   );
 }
