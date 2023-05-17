4cce0dd226eeddb6cfc746744c7ec0174b59148b
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-17 17:14:25 +0300

    add store to root

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import debounce from 'lodash.debounce';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './components/App/App';
 import { theme } from './themes';
+import { saveState } from './app/localStorage';
+import { store } from './app/store';
 import './index.css';
+
+store.subscribe(debounce(() => {
+  saveState(store.getState());
+}, 1000));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
