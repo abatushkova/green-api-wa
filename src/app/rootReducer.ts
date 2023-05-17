0fa26540face29ef08289a0f2f06e574a980c9e7
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-17 21:31:25 +0300

    add contacts reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { authReducer } from "../features/auth/authSlice";
+import { contactsReducer } from "../features/contacts/contactsSlice";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  contacts: contactsReducer,
 });
