3b65d2f849b8b0d99e099f8a57c70f972024ce33
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-17 17:19:43 +0300

    add logged-in condition to app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, CssBaseline } from '@mui/material';
 import Auth from '../Auth/Auth';
 import Main from '../Main/Main';
+import { useAppSelector } from '../../app/hooks';
 
 export default function App() {
+  const authState = useAppSelector(state => state.auth);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (authState.idInstance && authState.apiTokenInstance) {
+      setIsLoggedIn(true);
+      return;
+    }
+
+    setIsLoggedIn(false);
+  }, [authState]);
+
   return (
     <Box sx={{ display: 'flex', minHeight: '100vh' }}>
       <CssBaseline />
-      {/* <Auth /> */}
-      <Main />
+      {isLoggedIn ? (
+        <Main />
+      ) : (
+        <Auth />
+      )}
     </Box>
   );
 }
