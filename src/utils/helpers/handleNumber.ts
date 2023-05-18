889f18e5fcfad41d605f76eda2fad49e7d405bc2
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-18 23:59:10 +0300

    fix helper-handle-number funcs

--- src/utils/helpers/handleNumber.ts
+++ src/utils/helpers/handleNumber.ts
@@ -1,16 +1,13 @@
-import { loadState } from '../../app/localStorage';
-
 export const checkNumber = (number: string): boolean => {
-  const localContacts = loadState().chat.chatList;
-  const phoneNumbers = localContacts.map((item: any) => item.phoneNumber);
-
-  return !!(/^(\+7|8)\d{10}$/.exec(number.trim())) && !phoneNumbers.includes(number);
+  return !!(/^(\+7|8)\d{10}$/.exec(number.trim()));
 };
 
 export const createNewNumber = (number: string): string => {
-  if (/^\+7\d{10}$/.exec(number.trim())) {
-    return number.trim().replace('+7', '8');
+  const validNumber = number.trim();
+
+  if (/^8\d{10}$/.exec(validNumber)) {
+    return validNumber.replace('8', '7');
   }
 
-  return number;
-}
+  return validNumber.slice(1);
+};
