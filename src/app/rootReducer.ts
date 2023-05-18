cb902f700222bed3984f31b10e5661e62d8b0697
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-18 15:45:48 +0300

    add chat-reducer to root-reducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { authReducer } from "../features/auth/authSlice";
-import { contactsReducer } from "../features/contacts/contactsSlice";
+import { chatReducer } from "../features/chat/chatSlice";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
-  contacts: contactsReducer,
+  chat: chatReducer,
 });
