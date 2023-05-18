20a9e111ea6c786110e7491a27344ff06fafb875
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-18 12:42:55 +0300

    add inactive-chat base component

---
+++ src/components/Base/Base.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+export default function Base() {
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      flexGrow={1}
+    >
+      <Grid item>
+        <WhatsAppIcon color="secondary" sx={{ fontSize: 64 }} />
+      </Grid>
+    </Grid>
+  )
+}
