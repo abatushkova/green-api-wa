525a5b0ef77b6637bc7cdaedbb329d14a550bb6d
Alena Batushkova <alena.batushkova@gmail.com>
2023-05-19 20:50:57 +0300

    fix message-item styles

--- src/components/MessageItem/MessageItem.tsx
+++ src/components/MessageItem/MessageItem.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
+import { styled, useTheme } from '@mui/material/styles';
 import { Paper } from '@mui/material';
 
 const CustomItem = styled(Paper)(({ theme }) => ({
   ...theme.typography.body1,
   paddingInline: theme.spacing(2),
   paddingBlock: theme.spacing(1),
+  minHeight: theme.spacing(5),
   maxWidth: '80%',
   [theme.breakpoints.up('lg')]: {
     maxWidth: '60%',
   },
-  backgroundColor: theme.palette.secondary.main,
 }));
 
 interface MessageProps {
@@ -20,11 +20,12 @@ interface MessageProps {
 
 export default function MessageItem(props: MessageProps) {
   const { isOut, text } = props;
+  const theme = useTheme();
 
   return (
     <CustomItem sx={{
       alignSelf: isOut ? 'flex-end' : 'flex-start',
-      bgcolor: !isOut ? '#fff' : ''
+      bgcolor: isOut ? `${theme.palette.secondary.main}` : `${theme.palette.white.main}`
     }}>
       {text}
     </CustomItem>
